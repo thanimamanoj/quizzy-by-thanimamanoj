f19d796daa32bfb9d7d49e45d90d64f24b501d99
Thanima Manoj <thanimamanoj44@gmail.com>
2021-11-02 16:04:39 +0530

    Added  unit test for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  validates :first_name, presence: true, length: { maximum: Constants::MAX_TASK_NAME_LENGTH }
-  validates :last_name, presence: true, length: { maximum: Constants::MAX_TASK_NAME_LENGTH }
-  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true, length: { maximum: Constants::MAX_TASK_FIRST_NAME_LENGTH }
+  validates :last_name, presence: true, length: { maximum: Constants::MAX_TASK_LAST_NAME_LENGTH }
+  validates :email, presence: true, uniqueness: true, format: { with: Constants::VALID_EMAIL_REGEX }
+
+  before_save :to_lowercase
+
+  private
+
+  def to_lowercase
+    email.downcase!
+  end
 end
