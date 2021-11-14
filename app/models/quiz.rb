e78a825a54f0602ef11a30a530f2e8823babf8f6
Thanima Manoj <thanimamanoj44@gmail.com>
2021-11-14 17:51:55 +0530

    Added slug to quiz

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -5,4 +5,27 @@ class Quiz < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
+  validates :slug, uniqueness: true
+  validate :slug_not_changed
+
+  before_create :set_slug
+
+  private
+
+    def set_slug
+      itr = 1
+      loop do
+        title_slug = name.parameterize
+        slug_candidate = itr > 1 ? "#{title_slug}-#{itr}" : title_slug
+        break self.slug = slug_candidate unless Quiz.exists?(slug: slug_candidate)
+
+        itr += 1
+      end
+    end
+
+    def slug_not_changed
+      if slug_changed? && self.persisted?
+        errors.add(:slug, t("quiz.slug.immutable"))
+      end
+    end
 end
